ed1f820b17a078dd426f79ea62f3650715663088
Vladimir <vffkiv@gmail.com>
2017-02-09 20:02:06 +0200

    Initialization of GIt repository

---
+++ Folder 1/subFile1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vivanovs
+ * Date: 09.02.2017
+ * Time: 8:00 PM
+ */
+
+ echo 'initial sub fike1';
+ ?>
---
+++ test_1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vivanovs
+ * Date: 09.02.2017
+ * Time: 7:56 PM
+ */
+
+echo 'Initial git project';
+
+ ?>
